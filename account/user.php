b36e2e9917fd109b82ea61a80feb0916eb34e036
deklaswas <41451225+deklaswas@users.noreply.github.com>
2024-06-16 09:19:06 -0400

    Update user.php

--- account/user.php
+++ account/user.php
@@ -108,8 +108,7 @@ function test_input($data) {
   const ctx = c.getContext("2d");
 
 
-  ctx.beginPath();
-  ctx.rect(20, 20, 150, 100);
+  ctx.fillRect(20, 20, 150, 100);
   ctx.stroke();
 
   ctx.font = "20px Georgia";
