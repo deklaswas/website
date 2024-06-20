53b5c9b718454c81b751fd1b9fc37d5caba9e933
deklaswas <41451225+deklaswas@users.noreply.github.com>
2024-06-20 10:07:28 -0400

    Update user.php

--- account/user.php
+++ account/user.php
@@ -287,7 +287,7 @@ function test_input($data) {
     var avatarString = "";
     for (let i = 0; i < 8; i++) {
       for (let j = 0; j < 8; j++) {
-        avatarString += avatar[i][j];
+        avatarString += avatar[j][i];
       }
     }
 
