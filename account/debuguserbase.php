6d876bb80bea79f3871e5061421464ddbdff4f26
deklaswas <41451225+deklaswas@users.noreply.github.com>
2024-07-05 15:43:49 -0400

    Update debuguserbase.php

--- account/debuguserbase.php
+++ account/debuguserbase.php
@@ -12,7 +12,7 @@ $sql = 'SELECT * FROM users';
 foreach ($db->query($sql) as $row) {
     print_r($row);
     echo '<br/>';
-    $hasho = password_hash($row['password'], PASSWORD_BCRYPT, ["cost" => 12]);
+    $hasho = password_hash($row['password'], PASSWORD_BCRYPT, ["cost" => 11]);
     print_r( $hasho );
     echo '<br/>';
     if (password_verify($row['password'], $hasho)) {
