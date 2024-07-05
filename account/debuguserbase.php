a9d05b0cca425358432f3a30de973215089f16f9
deklaswas <41451225+deklaswas@users.noreply.github.com>
2024-07-05 15:41:32 -0400

    Update debuguserbase.php

--- account/debuguserbase.php
+++ account/debuguserbase.php
@@ -12,37 +12,20 @@ $sql = 'SELECT * FROM users';
 foreach ($db->query($sql) as $row) {
     print_r($row);
     echo '<br/>';
-    print_r( password_hash($row['password'], PASSWORD_DEFAULT ));
+    $hasho = password_hash($row['password'], PASSWORD_BCRYPT, ["cost" => 13]);
+    print_r( $hasho );
+    echo '<br/>';
+    if (password_verify($row['password'], $hasho)) {
+        echo 'Password is valid!';
+    } else {
+        echo 'Invalid password.';
+    }
     echo '<br/>';
     echo '<br/>';
 }
 
 
 ?>
-
-
-
-<?php
-/**
- * This code will benchmark your server to determine how high of a cost you can
- * afford. You want to set the highest cost that you can without slowing down
- * you server too much. 10 is a good baseline, and more is good if your servers
- * are fast enough. The code below aims for ≤ 350 milliseconds stretching time,
- * which is an appropriate delay for systems handling interactive logins.
- */
-$timeTarget = 0.350; // 350 milliseconds
-
-$cost = 10;
-do {
-    $cost++;
-    $start = microtime(true);
-    password_hash("test", PASSWORD_BCRYPT, ["cost" => $cost]);
-    $end = microtime(true);
-} while (($end - $start) < $timeTarget);
-
-echo "Appropriate Cost Found: " . $cost;
-?>
-
 
 </body>
 </html>
