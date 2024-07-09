78626e7c17a2fe6d712d822ef8776930fc861e53
deklaswas <41451225+deklaswas@users.noreply.github.com>
2024-07-09 13:34:41 -0400

    Update aboutme.php

--- account/edit/aboutme.php
+++ account/edit/aboutme.php
@@ -29,10 +29,6 @@ session_start();
 
 
 <?php
-
-// define variables and set to empty values
-$name = $password = "";
-
 //avatar array
 $aboutme = "";
 
@@ -96,151 +92,21 @@ function test_input($data) {
   <ul>
     <li>- Only letters, numbers, dashes, and whitespaces will be submitted.</li>
     <li>- 100 characters or less.</li>
-    <li>- Clicking "Clear" will paint the entire canvas with your selected color.</li>
+    <li>- No hateful writing please!</li>
   </ul>
 
 </p>
 
 <form  method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
   <div class='parent'>
-    <canvas 
-      id="avatarCanvas"
-      width="160"
-      height="160"
-      style="border:1px solid grey; display: inline-block;">
-    </canvas>
-    <div style='display: inline-block; vertical-align: text-bottom;'>
-      <button class="colbutton" type="button" onclick='paintColor = "0"'>Black</button>
-      <button class="colbutton" type="button" onclick='paintColor = "1"'>White</button><br>
-      <button class="colbutton" type="button" onclick='paintColor = "2"'>Red</button>
-      <button class="colbutton" type="button" onclick='paintColor = "3"'>Blue</button><br>
-      <button class="colbutton" type="button" onclick='paintColor = "4"'>Lime</button>
-      <button class="colbutton" type="button" onclick='paintColor = "5"'>Cyan</button><br>
-      <button class="colbutton" type="button" onclick='paintColor = "6"'>Magenta</button>
-      <button class="colbutton" type="button" onclick='paintColor = "7"'>Yellow</button><br>
-      <button class="colbutton" type="button" onclick='paintColor = "8"'>Brown</button>
-      <button class="colbutton" type="button" onclick='paintColor = "9"'>Green</button><br>
-      <br>
-      <button class="colbutton" type="button" onclick='clearCanvas()'>Clear</button>
-      <button class="colbutton" type="button" onclick='clearCanvas()'>Clear</button><br>
-      <button class="colbutton" type="submit" onclick='submitAvatar();'>Submit</button>
+    
+      
+      <textarea name="status" rows="5" cols="40"> <?php echo $aboutme;?> </textarea>
 
-
-    </div>
-    <br>
       <input id="avatarInput" type="text" name="avatarString" maxlength="64" minlength="64" size="64" style="font-size:0.59em" value="<?php echo $avatarString;?>"> 
   </div>
 </form>
 
-<script>
-
-  //canvas for editor
-  const c = document.getElementById("avatarCanvas");
-  const ctx = c.getContext("2d");
-
-  var textFieldAvatar = document.getElementById("avatarInput");
-
-  var avatar = <?php echo json_encode($avatar);?> ;
-  
-  var drawStyle = "pencil";
-
-  var paintColor = "0";
-
-
-  var mousePressed = -1;
-  var eventMouse;
-
-
-  function drawCanvas() {
-    const rect = c.getBoundingClientRect()
-    const x = Math.floor((eventMouse.clientX - rect.left)/20)
-    const y = Math.floor((eventMouse.clientY - rect.top)/20)
-
-    avatar[ x ][ y ] = paintColor;
-    drawAvatar(ctx,160);
-    
-    console.log(mousePressed);
-  }
-
-  function clearCanvas() {
-    for (let i = 0; i < 8; i++) {
-      for (let j = 0; j < 8; j++) {
-        avatar[i][j] =paintColor;
-      }
-    }
-    drawAvatar(ctx,160);
-  }
-
-  //setInterval( drawCanvas(), 100);
-  c.addEventListener('mouseon', function(e) {
-    eventMouse = e;
-    //if (mousePressed == -1)
-    drawCanvas()
-    mousePressed = setInterval(drawCanvas,100);
-    console.log(mousePressed);
-  })
-
-  //mouse up- start drawing
-  c.addEventListener('mousedown', function(e) {
-    eventMouse = e;
-    drawCanvas();
-    mousePressed = setInterval(drawCanvas,100);
-    console.log(mousePressed);
-  })
-  
-  //mouse up- stop drawing
-  c.addEventListener('mouseup', function(e) {
-    clearInterval(mousePressed);
-    mousePressed = -1;
-  })
-
-  function colorGrab(c) {
-    switch ( String(c) ) {
-      case "0": return "black";     // user
-      case "1": return "white";     //
-      case "2": return "red";       // playtester
-      case "3": return "blue";      // verified
-      case "4": return "lime";      //
-      case "5": return "cyan";      //
-      case "6": return "magenta";   // moderator
-      case "7": return "yellow";    //
-      case "8": return "sienna";    // poopy
-      case "9": return "green";     // owner
-    }
-  }
-
-  function drawAvatar(contextDraw,sizeo) {
-    sizeo /= 8;
-    var valo = "";
-    for (let i = 0; i < 8; i++) {
-      for (let j = 0; j < 8; j++) {
-        contextDraw.fillStyle = colorGrab(avatar[i][j]);
-        contextDraw.fillRect(i*sizeo, j*sizeo, sizeo, sizeo);
-
-        valo += avatar[j][i];
-      }
-    }
-    textFieldAvatar.value = valo;
-  }
-
-  drawAvatar(ctx,160);
-
-  function submitAvatar() {
-    drawAvatar(pctx,80);
-
-    //turn avatar into string
-    var avatarString = "";
-    for (let i = 0; i < 8; i++) {
-      for (let j = 0; j < 8; j++) {
-        avatarString += avatar[j][i];
-      }
-    }
-
-
-  }
-
-
-</script> 
 
 </body>
 </html>
