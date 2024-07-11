a8e876ee2e9e3f35b246d8748690692113f76341
deklaswas <41451225+deklaswas@users.noreply.github.com>
2024-07-10 23:15:02 -0400

    Update userboard.php

--- account/userboard.php
+++ account/userboard.php
@@ -106,7 +106,7 @@ echo "<script>console.log('Debug Objects: " . $userTable->top()["name"] . "' );<
 
   for (var i = 0; i < sizeList; i++) {
     //get data for profile
-    var profileData = <?php json_encode( $userTable->top() ); ?>
+    var profileData = <?php json_encode( $userTable->top()["name"] ); ?>
     console.log(profileData);
 
     //canvas for avatar
