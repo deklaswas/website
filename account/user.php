8a142fd0dfbf73dac7b26a40071111a21e35a06d
deklaswas <41451225+deklaswas@users.noreply.github.com>
2024-07-11 00:33:48 -0400

    Update user.php

--- account/user.php
+++ account/user.php
@@ -160,7 +160,7 @@ for ($i = 0; $i < count($avatar); $i++) {
       case "8": return "sienna";    // poopy
       case "9": return "green";     // owner
 
-      //case "?": return colorGrab(Math.floor(Math.random()*10).toString()); // wildcard
+      case "?": return colorGrab(Math.floor(Math.random()*10).toString()); // wildcard
     }
   }
   function roleGrab(c) {
