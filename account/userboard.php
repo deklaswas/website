88c1dc4b1839e033e123ea39a561d167b67ef45e
deklaswas <41451225+deklaswas@users.noreply.github.com>
2024-07-12 01:34:35 -0400

    Update userboard.php

--- account/userboard.php
+++ account/userboard.php
@@ -117,7 +117,7 @@ function colorGrab(c) {
 
     
     $borderColor = colorGrab( userTable[i].color );
-    if ($borderColor == "black" && $_SESSION["darkmode"]) $borderColor = "white";
+    if ($borderColor == "black" && <?php echo $_SESSION["darkmode"] ?> ) $borderColor = "white";
     pc.setAttribute("style", pc.getAttribute("style") + $borderColor + ";");
   }
 
