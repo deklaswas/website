74fbd0addd796616b8400383d8259d4e6be0a1b0
deklaswas <41451225+deklaswas@users.noreply.github.com>
2024-07-12 00:50:47 -0400

    Update settings.php

--- account/edit/settings.php
+++ account/edit/settings.php
@@ -55,8 +55,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ( !is_string($invert)) $invert = "0"; else $invert = "1";
 
     try {
-      //$sql = 'UPDATE users SET invert = "'. $invert . '" WHERE name = "' . $_SESSION["username"] . '";';
-      $sql = 'UPDATE users SET invert = "0" WHERE name = "deklaswas";';
+      $sql = 'UPDATE users SET invert = "'. $invert . '" WHERE name = "' . $_SESSION["username"] . '";';
       $db->exec($sql);
 
       header('Location: http://www.deklaswas.com/account/myaccount.php');
