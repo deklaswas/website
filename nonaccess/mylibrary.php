e003e8ae776baf124b20e737f6aa7d0fc789ecdf
deklaswas <deklaswas@gmail.com>
2024-09-22 01:50:36 +0000

    change php library location

---
+++ nonaccess/mylibrary.php
@@ -0,0 +1,25 @@
+<?php
+$colorText = ($_SESSION["darkmode"]) ? ("white") : ("black");
+$colorBackground = ($_SESSION["darkmode"]) ? ("black") : ("white");
+$colorString = "body { background-color:" . (($_SESSION["darkmode"]) ? ("black") : ("white")) . "; color:" . $colorText . ";}";
+
+$colorSwitch = '
+function colorGrab(c) {
+    switch ( String(c) ) {
+      case "0": return "black";     // user
+      case "1": return "white";     //
+      case "2": return "red";       // playtester
+      case "3": return "blue";      // verified
+      case "4": return "lime";      //
+      case "5": return "cyan";      //
+      case "6": return "magenta";   // moderator
+      case "7": return "yellow";    //
+      case "8": return "sienna";    // poopy
+      case "9": return "green";     // owner
+
+      case "?": return colorGrab(Math.floor(Math.random()*10).toString()); // wildcard
+      case ".": return "' . $colorBackground . '"; // transparent/background
+    }
+};
+'
+?>
